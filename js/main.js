6bc381a8826b69c061ab902af0cea34a7a10ec48
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-12-18 16:08:34 -0600

    build:Allow the user to play the user and get a result of the round

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 // Return a random choice for the computer
 function getComputerChoice() {
-    choices = ["Rock", "Paper", "Scissors"];
+    choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+// Compare the user option and the computer option
+function playerRound(playerSelection, computerSelection){
+    // Make the options case insensitive
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        return "Tie!";
+    }
+
+    // Player chose rock
+    if(playerSelection === "rock") {
+        if(computerSelection === "paper"){
+            return "You Lose! Paper beats Rock!"
+        } 
+        return "You Win! Rock beats Scissors!"
+    }
+
+    // Player chose paper
+    if(playerSelection === "paper") {
+        if(computerSelection === "scissors"){
+            return "You Lose! Scissors beats Paper!"
+        }
+        return "You Win! Paper beats Rock!"
+    }
+
+    // Player chose scissors
+    if(playerSelection === "scissors") {
+        if(computerSelection === "rock"){
+            return "You Lose! Rock beats Scissors!"
+        }
+        return "You Win! Rock beats Paper!"
+    }
+
+    return "Invalid Choice"
 }
