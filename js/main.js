e4c2a16602ec3e40eb2cb9e33ee4cb69d9ed3a80
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-12-18 16:24:23 -0600

    build:Run multiple rounds
    The user is able to run more than one round and
    there is a prompt asking the suer for their input.
    
    The user's score is also updated the more
    rounds the user wins

--- js/main.js
+++ js/main.js
@@ -11,32 +11,52 @@ function playerRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
-        return "Tie!";
+        return ["Tie!", 0];
     }
 
     // Player chose rock
     if(playerSelection === "rock") {
         if(computerSelection === "paper"){
-            return "You Lose! Paper beats Rock!"
+            return ["You Lose! Paper beats Rock!", 0]
         } 
-        return "You Win! Rock beats Scissors!"
+        return ["You Win! Rock beats Scissors!", 1]
     }
 
     // Player chose paper
     if(playerSelection === "paper") {
         if(computerSelection === "scissors"){
-            return "You Lose! Scissors beats Paper!"
+            return ["You Lose! Scissors beats Paper!", 0]
         }
-        return "You Win! Paper beats Rock!"
+        return ["You Win! Paper beats Rock!", 1]
     }
 
     // Player chose scissors
     if(playerSelection === "scissors") {
         if(computerSelection === "rock"){
-            return "You Lose! Rock beats Scissors!"
+            return ["You Lose! Rock beats Scissors!", 0]
         }
-        return "You Win! Rock beats Paper!"
+        return ["You Win! Rock beats Paper!", 1]
     }
 
-    return "Invalid Choice"
+    return ["Invalid Choice", 0]
 }
+
+function game() {
+    // Keep track of user's score
+    let score = 0
+    for(let i = 0; i < 5; i++){
+        // Get the user and computer selections
+        let userSelection = prompt("Pick (Rock, Paper or Scissors):")
+        let computerSelection = getComputerChoice()
+        
+        // Run the round
+        let [result, scoreAdder] = playerRound(userSelection, computerSelection)
+        score += scoreAdder
+
+        // Show result
+        alert(result)
+        alert(score)
+    }
+}
+
+game()
